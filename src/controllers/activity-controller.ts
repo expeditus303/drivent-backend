6558d216a23056bf2582bbcc23f48f0e3809f1a4
Davi Barci <davibarci0208@gmail.com>
2023-08-02 17:04:27 -0300

    feat: add validations against remote tickets and unpaid ticket

--- src/controllers/activity-controller.ts
+++ src/controllers/activity-controller.ts
@@ -3,18 +3,22 @@ import { Response } from 'express';
 import httpStatus from 'http-status';
 import activityService from '@/services/activities-service';
 
-export async function getDays(_req: AuthenticatedRequest, res: Response) {
+export async function getDays(req: AuthenticatedRequest, res: Response) {
+  const { userId } = req;
+
   try {
-    const days = await activityService.getDays();
+    const days = await activityService.getDays(userId);
     return res.status(httpStatus.OK).send(days);
   } catch (error) {
     return res.sendStatus(httpStatus.NOT_FOUND);
   }
 }
 
-export async function getLocations(_req: AuthenticatedRequest, res: Response) {
+export async function getLocations(req: AuthenticatedRequest, res: Response) {
+  const { userId } = req;
+
   try {
-    const locations = await activityService.getLocations();
+    const locations = await activityService.getLocations(userId);
     return res.status(httpStatus.OK).send(locations);
   } catch (error) {
     return res.sendStatus(httpStatus.NOT_FOUND);
@@ -22,8 +26,10 @@ export async function getLocations(_req: AuthenticatedRequest, res: Response) {
 }
 
 export async function getActivities(req: AuthenticatedRequest, res: Response) {
+  const { userId } = req;
+
   try {
-    const activities = await activityService.getActivities();
+    const activities = await activityService.getActivities(userId);
     return res.send(activities);
   } catch (error) {
     return res.sendStatus(httpStatus.NOT_FOUND);
